c14c88581cf07ff45442baf8b249a4a6c526064b
Mona Dasig <monalizadasig@gmail.com>
2025-06-10 13:13:08 -0700

    feat(api): add CRUD endpoints for Category
    - Implemented create, read, update, and delete routes for Category

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 import userRouter from './routes/userRoutes';
+import transactionRouter from './routes/transactionRoutes';
+import categoryRouter from './routes/categoryRoutes';
 import config from './config/config';
+import { verifyJWT } from './middlewares/authMiddleware';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/users', userRouter);
+app.use('/api/categories', verifyJWT, categoryRouter);
 
 app.listen(config.port, () => {
   console.log(`APP running on port ${config.port}.`);
--- server/src/utils/enums.ts
+++ server/src/utils/enums.ts
@@ -2,3 +2,8 @@ export enum WalletType {
   MAIN = 'main',
   SAVINGS = 'savings',
 }
+
+export enum CategoryType {
+  INCOME = 'income',
+  EXPENSE = 'expense',
+}
