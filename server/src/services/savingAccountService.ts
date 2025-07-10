09ce7029a0e073b4ce53484836b40681d23ceb69
Mona Dasig <monalizadasig@gmail.com>
2025-07-09 17:03:23 -0700

    refactor(api): omit get Savings Response userId

--- server/src/services/savingAccountService.ts
+++ server/src/services/savingAccountService.ts
@@ -21,6 +21,9 @@ export async function getAllSavings(userId: number) {
     include: {
       savingAccount: true,
     },
+    omit: {
+      userId: true,
+    },
   });
   return savingAccounts;
 }
