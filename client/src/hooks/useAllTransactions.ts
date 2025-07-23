0d90f969f7157f629a765bb680d4b02f3b6bbe48
Mona Dasig <monalizadasig@gmail.com>
2025-07-23 15:34:58 -0700

    feat: add hooks for fetching all transactions

---
+++ client/src/hooks/useAllTransactions.ts
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import {
+  getAllTransactions,
+  type AllTransactionsResponse,
+} from "../services/transactions";
+import type { DateRange } from "../pages/transaction/Transaction";
+
+export const useAllTransactions = (dateRange: DateRange) => {
+  const { data: transactions = [], isLoading } = useQuery<
+    AllTransactionsResponse[]
+  >({
+    queryKey: ["all-transactions", dateRange.startDate, dateRange.endDate],
+    queryFn: () => getAllTransactions(dateRange.startDate, dateRange.endDate),
+  });
+
+  return { transactions, isLoading };
+};
