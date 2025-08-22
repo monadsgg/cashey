d25c432ee9ebe43d6d559ce04f89944faa1ef57c
Mona Dasig <monalizadasig@gmail.com>
2025-08-22 13:21:27 -0700

    refactor(transaction): cache allTransactionsQuery with dateRange by using memo

--- client/src/hooks/transactions/useAllTransactions.ts
+++ client/src/hooks/transactions/useAllTransactions.ts
@@ -1,15 +1,21 @@
+import { useMemo } from "react";
 import { useQuery } from "@tanstack/react-query";
 import {
   getAllTransactions,
   type AllTransactionsResponse,
-} from "../services/transactions";
-import type { DateRange } from "../pages/transaction/Transaction";
+} from "../../services/transactions";
+import type { DateRange } from "../../pages/transaction/Transaction";
 
 export const useAllTransactions = (dateRange: DateRange) => {
+  const txQueryKey = useMemo(
+    () => ["all-transactions", dateRange.startDate, dateRange.endDate],
+    [dateRange.startDate, dateRange.endDate]
+  );
+
   const { data: transactions = [], isLoading } = useQuery<
     AllTransactionsResponse[]
   >({
-    queryKey: ["all-transactions", dateRange.startDate, dateRange.endDate],
+    queryKey: txQueryKey,
     queryFn: () => getAllTransactions(dateRange.startDate, dateRange.endDate),
   });
 
