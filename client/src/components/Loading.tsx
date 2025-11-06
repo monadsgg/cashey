db4a221a08ff9d459a7ec1c5efa68528389c9c00
Mona Dasig <monalizadasig@gmail.com>
2025-11-06 13:22:52 -0800

    refactor: update Loading component to accept message props

--- client/src/components/Loading.tsx
+++ client/src/components/Loading.tsx
@@ -1,7 +1,18 @@
+import Stack from "@mui/material/Stack";
 import CircularProgress from "@mui/material/CircularProgress";
+import Typography from "@mui/material/Typography";
 
-function Loading() {
-  return <CircularProgress />;
+interface LoadingProps {
+  message?: string;
+}
+
+function Loading({ message }: LoadingProps) {
+  return (
+    <Stack alignItems="center" mt={4}>
+      <CircularProgress />
+      {message && <Typography mt={2}>{message}</Typography>}
+    </Stack>
+  );
 }
 
 export default Loading;
